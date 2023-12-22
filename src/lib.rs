0640dfa14f5c5df9148d90a1f01b8284a4084b61
rohitverma007 <rohitverma@live.ca>
2023-12-22 23:56:55 +0000

    adding comments to contract file

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+// A simple Rock-Paper-Scissors contract that uses a PRNG to generate the contract's choice
+// and stores the stats in the contract's storage. Rock is represented by 1, Paper by 2 and
+// Scissors by 3.
 #![no_std]
 use soroban_sdk::{contract, contractimpl, symbol_short, Env, Symbol};
 
@@ -14,8 +17,11 @@ pub struct RPSContract;
 #[contractimpl]
 impl RPSContract {
     pub fn play(env: Env, player_choice: u64) -> u64 {
+        // We use a PRNG to generate the contract's choice
         let contract_choice = env.prng().gen_range(1..=3);
         let result = RPSContract::determine_winner(player_choice, contract_choice);
+
+        // We update the stats
         let mut p_wins: u64 = env.storage().instance().get(&P_WINS).unwrap_or(0);
         let mut c_wins: u64 = env.storage().instance().get(&C_WINS).unwrap_or(0);
         match result {
@@ -33,6 +39,7 @@ impl RPSContract {
     }
 
     pub fn determine_winner(player: u64, contract: u64) -> u64 {
+        // Depending on the choices, we determine the winner
         match (player, contract) {
             (1, 3) | (2, 1) | (3, 2) => 1, // Player wins
             (1, 2) | (2, 3) | (3, 1) => 2, // Contract wins
